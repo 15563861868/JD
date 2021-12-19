bed2080910091c0eb5c0e63ad5e62eb067a746c7
Phyllis <1363385560@qq.com>
2021-12-19 20:00:34 +0800

    12.19第三提交

--- js/detail.js
+++ js/detail.js
@@ -1,10 +1,17 @@
 let small = document.querySelector('.mid_photo');
+let smallbox = document.querySelector('.mid_photo div')
 // console.log(small.offsetTop);
 let mask = document.querySelector('.mask');
 let big = document.querySelector('.big_photo');
-let pic = document.querySelector('.big_photo>img');
-let smallbox = document.querySelector('.mid_photo>img');
-let btn = document.querySelectorAll('.preview_list .list_item .list_item_menu li');
+let bigBox = document.querySelector('.big_photo div');
+
+let picAll=document.querySelectorAll('.big_photo>div');
+let smallpic = document.querySelectorAll('.mid_photo>div');
+// console.log(smallpic);
+let btn = document.querySelectorAll('.preview_list .list_item li img');
+let li = document.querySelectorAll('.preview_list .list_item .list_item_menu li')
+// let ul=document.querySelector('.preview_list .list_item');
+
 small.onmouseover = () => {
     mask.style.display = 'block';
     big.style.display = 'block';
@@ -13,15 +20,39 @@ small.onmouseover = () => {
 small.onmouseout = () => {
     mask.style.display = 'none';
     big.style.display = 'none';
+    // console.log(123);
 }
+for (let i = 0; i < btn.length; i++) {
+    // console.log(btn.length);
+    btn[i].onclick = () => {
+        clean();
+        // let src = btn[i].getAttribute('src');
+        // console.log(src);
+        smallpic[i].style.display='block';
+        // console.log(smallpic[i]);
+        picAll[i].style.display='block';
+        // console.log(picAll[i]);
+        picAll[i].className='showOut';
+    }
+}
+for (let k = 0; k < li.length; k++) {
+    li[k].onclick = function(){
+        clear();
+        // console.log(this);
+        this.className = 'liActive';
+    }
+}
+
+
+// console.log(pic);
 small.onmousemove = (e) => {
     e = e || window.event
-    let x = e.pageX - small.offsetLeft - mask.offsetWidth / 2
+    let x = e.pageX - small.offsetParent.offsetLeft - mask.offsetWidth / 2
     let y = e.pageY - small.offsetTop - mask.offsetHeight / 2
     if (x <= 0) {
         x = 0
-    } else if (x >= smallbox.offsetWidth - mask.offsetWidth) {
-        x = smallbox.offsetWidth - mask.offsetWidth
+    } else if (x >= small.offsetWidth - mask.offsetWidth) {
+        x = small.offsetWidth - mask.offsetWidth
     }
     if (y <= 0) {
         y = 0
@@ -30,8 +61,92 @@ small.onmousemove = (e) => {
     }
     mask.style.left = x + 'px'
     mask.style.top = y + 'px'
-    let w = x / (smallbox.offsetWidth - mask.offsetWidth)
-    let h = y / (smallbox.offsetHeight - mask.offsetHeight)
+    let w = x / (parseInt(getComputedStyle(smallpic[0]).width) - mask.offsetWidth)
+    // console.log(smallpic[0].offsetWidth);
+    let h = y / (parseInt(getComputedStyle(smallpic[0]).height)  - mask.offsetHeight)
+    // console.log(smallpic[0].offsetWidth - mask.offsetWidth);
+    // console.log(w);
+    let pic = document.querySelector('.showOut');
+    let img=pic.querySelector('img')
+    // console.log(img);
+    // console.log(pic,pic.offsetWidth,big.offsetWidth);
+    // console.log(w);
     pic.style.left = -w * (pic.offsetWidth - big.offsetWidth) + 'px'
     pic.style.top = -h * (pic.offsetHeight - big.offsetHeight) + 'px'
+    // console.log(pic.offsetWidth - big.offsetWidth);
+}
+
+let ul=document.querySelector('.preview_list .list_item');
+let col=document.querySelectorAll('.preview_list .list_item .list_item_menu');
+let right=document.querySelector('.arrow_next');
+let i=3;
+right.onclick = () => {
+    if(i%2==0){
+        col[1].style.display='none';
+        col[0].style.display='block';
+    }else{
+        col[0].style.display='none';
+        col[1].style.display='block';
+        // console.log(col[1]);
+    }
+    i++
+}
+let left=document.querySelector('.arrow_prev');
+// let k=-3;
+left.onclick=()=>{
+    if(i%2==0){
+        col[1].style.display='none';
+        col[0].style.display='block';
+    }else{
+        col[0].style.display='none';
+        col[1].style.display='block';
+    }
+    i--
+}
+
+let hov=document.querySelectorAll('.nav_tab .nav_tab_item h2');
+let show=document.querySelectorAll('.nav_tab .nav_tab_item .item_tab_msg');
+for(let i=0;i<hov.length;i++){
+    hov[i].onmouseenter=()=>{
+        // console.log(i);
+        show[i].style.display='show';
+        // console.log(show[i]);
+    }
+    hov[i].onmouseleave=()=>{
+        // console.log(i);
+        show[i].style.display='none';
+    }
+}
+
+let label = document.querySelectorAll('.nav_tab_list .nav_tab_item');
+ let content = document.querySelectorAll('.item_menu .row');
+ // 鼠标移入tab栏，显示对应子tab栏内容
+ for(let i=0;i<label.length;i++){
+    label[i].onclick = function(){
+        clearName();
+        content[i].style.display = 'block';
+    }
+ }
+
+ function clearName() {
+     content.forEach(item => {
+         item.style.display = 'none';
+     })
+ }
+
+function clear(){
+    btn.forEach(item=>{
+        item.className='';
+    })
+}
+
+function clean(){
+    // console.log(111);
+    smallpic.forEach(item=>{
+        item.style.display='none';
+    })
+    picAll.forEach(item=>{
+        item.style.display='none';
+        item.className='';
+    })
 }
--- js/index.js
+++ js/index.js
@@ -4,43 +4,43 @@
  }
 
  // 轮播
- let timer='';
- timer=setInterval(function(){
-    let show = _$('.content .show');
-    if (num < lis.length - 1) {
-        show.className = '';
-        show.nextElementSibling.className = 'show';
-        num++;
-    } else {
-        num = 0;
-        lis.forEach(item => {
-            item.className = ''
-        })
-        one.className = 'show';
-    }
-    active();
-},2000);
- _$('.content').onmouseover=()=>{
-    clearInterval(timer);
+ let timer = '';
+ timer = setInterval(function () {
+     let show = _$('.content .show');
+     if (num < lis.length - 1) {
+         show.className = '';
+         show.nextElementSibling.className = 'show';
+         num++;
+     } else {
+         num = 0;
+         lis.forEach(item => {
+             item.className = ''
+         })
+         one.className = 'show';
+     }
+     active();
+ }, 2000);
+ _$('.content').onmouseover = () => {
+     clearInterval(timer);
  }
- _$('.content').onmouseout=()=>{
-    timer=setInterval(function(){
-        let show = _$('.content .show');
-        if (num < lis.length - 1) {
-            show.className = '';
-            show.nextElementSibling.className = 'show';
-            num++;
-        } else {
-            num = 0;
-            lis.forEach(item => {
-                item.className = ''
-            })
-            one.className = 'show';
-        }
-        active();
-    },2000);
+ _$('.content').onmouseout = () => {
+     timer = setInterval(function () {
+         let show = _$('.content .show');
+         if (num < lis.length - 1) {
+             show.className = '';
+             show.nextElementSibling.className = 'show';
+             num++;
+         } else {
+             num = 0;
+             lis.forEach(item => {
+                 item.className = ''
+             })
+             one.className = 'show';
+         }
+         active();
+     }, 2000);
  }
- let show =_$('.content .show');
+ let show = _$('.content .show');
  let lis = $$('.content li');
 
  let one = lis[0];
@@ -49,26 +49,26 @@
  let num = 0;
  let next = _$(".btn_r");
  let prev = _$(".btn_l");
-  // next按钮
-    next.onclick = () => {
-        let show = _$('.content .show');
-        if (num < lis.length - 1) {
-            show.className = '';
-            show.nextElementSibling.className = 'show';
-            num++;
-        } else {
-            num = 0;
-            lis.forEach(item => {
-                item.className = ''
-            })
-            one.className = 'show';
-        }
-        active();
-    }
+ // next按钮
+ next.onclick = () => {
+     let show = _$('.content .show');
+     if (num < lis.length - 1) {
+         show.className = '';
+         show.nextElementSibling.className = 'show';
+         num++;
+     } else {
+         num = 0;
+         lis.forEach(item => {
+             item.className = ''
+         })
+         one.className = 'show';
+     }
+     active();
+ }
  // previous按钮
  prev.onclick = () => {
      let show = _$('.content .show');
-    //  let last = lis[lis.length - 1];
+     //  let last = lis[lis.length - 1];
      // console.log(last);
      if (num > 0) {
          show.className = '';
@@ -96,7 +96,7 @@
  }
  let btns = $$('.dotted li')
  btns[0].className = 'active';
-//  console.log(btns);
+ //  console.log(btns);
 
  function active() {
      btns.forEach(item => {
@@ -110,61 +110,82 @@
  }
 
 
-//  倒计时
-setInterval(time,1000);
-function time(){
-    let endDate=new Date('2021-12-24 18:00:00').getTime();
-    // 现在的时间
-    let nowTime=new Date().getTime();
-    // 总秒数
-    let time=parseInt((endDate-nowTime)/1000);
-    let h=complement(parseInt(time/3600%24));
-    let m=complement(parseInt(time/60%60));
-    let s=complement(parseInt(time%60));
-    _$('.time_count .count_time .hour').innerHTML=h;
-    _$('.count_time .minute').innerHTML=m;
-    _$('.time_count .count_time .seconds').innerHTML=s;
-}
-time();
-function complement(num){
-    return num < 10 ? num = '0' + num : num
-}
+ //  倒计时
+ setInterval(time, 1000);
 
-// 秒杀手动轮播
-let ul=_$('.miaosha_content');
-let col=$$('.miaosha_content .col');
-// console.log(col);
-let right=_$('.miaosha_lunbo .btn_r');
-let i=0;
-right.onclick = () => {
-    if(i<col.length-1){
-        i+=1;
-        ul.style.left=-(800*i)+'px';
-    }else if(i==col.length-1){
-        i=0;
-        ul.style.left=-(800*i)+'px';
+ function time() {
+     let endDate = new Date('2021-12-24 18:00:00').getTime();
+     // 现在的时间
+     let nowTime = new Date().getTime();
+     // 总秒数
+     let time = parseInt((endDate - nowTime) / 1000);
+     let h = complement(parseInt(time / 3600 % 24));
+     let m = complement(parseInt(time / 60 % 60));
+     let s = complement(parseInt(time % 60));
+     _$('.time_count .count_time .hour').innerHTML = h;
+     _$('.count_time .minute').innerHTML = m;
+     _$('.time_count .count_time .seconds').innerHTML = s;
+ }
+ time();
+
+ function complement(num) {
+     return num < 10 ? num = '0' + num : num
+ }
+
+ // 秒杀手动轮播
+ let ul = _$('.miaosha_content');
+ let col = $$('.miaosha_content .col');
+ // console.log(col);
+ let right = _$('.miaosha_lunbo .btn_r');
+ let i = 0;
+ right.onclick = () => {
+     if (i < col.length - 1) {
+         i += 1;
+         ul.style.left = -(800 * i) + 'px';
+     } else if (i == col.length - 1) {
+         i = 0;
+         ul.style.left = -(800 * i) + 'px';
+     }
+ }
+ let left = _$('.miaosha_lunbo .btn_l');
+ left.onclick = () => {
+     if (i > 0) {
+         i -= 1;
+         ul.style.left = -(800 * i) + 'px';
+     } else {
+         i = col.length - 1;
+         ul.style.left = -(800 * i) + 'px';
+     }
+ }
+
+ // 我的购物车
+ // let goodsNum=_$('.shop_car .num');
+ // let goods=JSON.parse(localStorage.getItem('cart'));
+ // let goods=localStorage.getItem('cart');
+ // console.log(goods);
+
+ // 为你推荐tab切换
+ let label = document.querySelectorAll('.for_you_menu .for_tab_index');
+ let content = document.querySelectorAll('.for_you_tab');
+ let tabBtn=document.querySelectorAll('.for_you_menu .for_tab_index a');
+ // 鼠标移入tab栏，显示对应子tab栏内容
+ for(let i=0;i<label.length;i++){
+    label[i].onclick = function(){
+        console.log(111);
+        clearName();
+        tabBtn[i].classList.add('for_you_current');
+        content[i].style.display = 'block';
     }
-}
-let left=_$('.miaosha_lunbo .btn_l');
-left.onclick=()=>{
-    if(i>0){
-        i-=1;
-        ul.style.left=-(800*i)+'px';
-    }else{
-        i=col.length-1;
-        ul.style.left=-(800*i)+'px';
-    }
-}
+ }
 
-// 我的购物车
-// let goodsNum=_$('.shop_car .num');
-// let goods=JSON.parse(localStorage.getItem('cart'));
-// let goods=localStorage.getItem('cart');
-// console.log(goods);
-
-// 加入购物车
-let enter=$$('.lookfor_sim .xiangsi');
-// console.log(enter);
+ function clearName() {
+     tabBtn.forEach(item => {
+         item.classList.remove('for_you_current');
+     })
+     content.forEach(item => {
+         item.style.display = 'none';
+     })
+ }
 
 
 
